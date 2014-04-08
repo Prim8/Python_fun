ac15580d876b8e1d710a4089b8d13ed559d25447
Prim8 <bapik001@umn.edu>
2014-04-08 15:49:50 -0500

    Add list1.py

---
+++ list1.py
@@ -0,0 +1,85 @@
+# List exercises
+
+# A. match_ends
+# Given a list of strings, return the count of the number of
+# strings where the string length is 2 or more and the first
+# and last chars of the string are the same.
+
+def match_ends(words):
+    num_str = 0
+    for s in words:
+        if len(s) >= 2 and s[0] == s[-1]:
+            num_str += 1
+    return num_str 
+    
+
+# B. front_x
+# Given a list of strings, return a list with the strings
+# in sorted order, except group all the strings that begin with 'x' first.
+# e.g. ['mix', 'xyz', 'apple', 'xanadu', 'aardvark'] yields
+# ['xanadu', 'xyz', 'aardvark', 'apple', 'mix']
+
+def front_x(words):
+    x_list = []
+    not_x_list = []
+    for i in words:
+        if i[0] == 'x':
+            x_list.append(i)
+        else:
+            not_x_list.append(i)
+    x_list.sort()
+    not_x_list.sort()
+    x_list.extend(not_x_list)
+    return x_list
+
+
+# C. sort_last
+# Given a list of non-empty tuples, return a list sorted in increasing
+# order by the last element in each tuple.
+# e.g. [(1, 7), (1, 3), (3, 4, 5), (2, 2)] yields
+# [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
+
+def sort_last(tuples):
+    def last_element(t):
+      return t[-1]
+    return sorted(tuples, key=last_element)
+
+# Simple provided test() function used in main() to print
+# what each function returns vs. what it's supposed to return.
+def test(got, expected):
+  if got == expected:
+    prefix = ' OK '
+  else:
+    prefix = '  X '
+  print '%s got: %s expected: %s' % (prefix, repr(got), repr(expected))
+
+
+# Calls the above functions with interesting inputs.
+def main():
+  print 'match_ends'
+  test(match_ends(['aba', 'xyz', 'aa', 'x', 'bbb']), 3)
+  test(match_ends(['', 'x', 'xy', 'xyx', 'xx']), 2)
+  test(match_ends(['aaa', 'be', 'abc', 'hello']), 1)
+
+  print
+  print 'front_x'
+  test(front_x(['bbb', 'ccc', 'axx', 'xzz', 'xaa']),
+       ['xaa', 'xzz', 'axx', 'bbb', 'ccc'])
+  test(front_x(['ccc', 'bbb', 'aaa', 'xcc', 'xaa']),
+       ['xaa', 'xcc', 'aaa', 'bbb', 'ccc'])
+  test(front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark']),
+       ['xanadu', 'xyz', 'aardvark', 'apple', 'mix'])
+
+       
+  print
+  print 'sort_last'
+  test(sort_last([(1, 3), (3, 2), (2, 1)]),
+       [(2, 1), (3, 2), (1, 3)])
+  test(sort_last([(2, 3), (1, 2), (3, 1)]),
+       [(3, 1), (1, 2), (2, 3)])
+  test(sort_last([(1, 7), (1, 3), (3, 4, 5), (2, 2)]),
+       [(2, 2), (1, 3), (3, 4, 5), (1, 7)])
+
+
+if __name__ == '__main__':
+  main()
